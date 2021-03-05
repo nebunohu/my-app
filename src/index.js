35e61d6b292dde424c9a68bc0ab6f3f1a75e4f3a
Barashkov <nebunohu@gmail.com>
2021-03-05 16:35:47 +0300

    Intermediate commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
